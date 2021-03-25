7e2d9ee8614f7e1e0c4f68ee6bdf6edd8e8c3558
carlos <carlosmast2301@hotmail.es>
2021-03-24 19:02:11 -0500

    a

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://www.repuestoscar.codesoft-ec.com/';
+$config['base_url']	= 'http://www.repuestoscar.codesoft-ec.com/index.php/';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ $config['base_url']	= 'http://www.repuestoscar.codesoft-ec.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
